6c0094f7ded79e7c57bd352427e80b5553794c14
tsluder <taylor@tsluder.dev>
2022-11-09 07:35:05 -0500

    remove test for letter type and stabilize array variables

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-insultAdj1 = ['hateful', 'nasty', 'common', 'bloody', 'vacant', 'knavish', 'quarrelsome', 'grotesque', 'revolting', 'unkind',
+const insultAdj1 = ['hateful', 'nasty', 'common', 'bloody', 'vacant', 'knavish', 'quarrelsome', 'grotesque', 'revolting', 'unkind',
             'saucy', 'disgusting', 'grizzled', 'spotted', 'cantankerous', 'lewd', 'loathsome', 'brazen', 'detestable',
             'reeky', 'repugnant', 'abominable', 'peevish', 'greasy'];
 
-insultAdj2 = ['ill-breeding', 'low-spirited', 'cold-blooded', 'luggage-lipped', 'double-dealing', 'pale-hearted', 'sallow-skinned',
+const insultAdj2 = ['ill-breeding', 'low-spirited', 'cold-blooded', 'luggage-lipped', 'double-dealing', 'pale-hearted', 'sallow-skinned',
             'puke-stained', 'green-eyed', 'lily-livered', 'eye-offending', 'long-winded', 'pasty-faced', 'dark-hearted', 'low-down',
             'wax-eared', 'onion-breathing', 'paste-brained', 'broke-dealing', 'dull-spirited', 'stone-faced', 'black-winged',
             'dull-witted', 'poison-tongued'];
 
-insultNoun = ['laughingstock', 'twit', 'ruffian', 'pirate', 'buffoon', 'boar', 'maggot-pie', 'hog', 'scoundrel', 'dolt', 'jack-a-nape',
+const insultNoun = ['laughingstock', 'twit', 'ruffian', 'pirate', 'buffoon', 'boar', 'maggot-pie', 'hog', 'scoundrel', 'dolt', 'jack-a-nape',
             'serpent', 'dullard', 'devil', 'dung-hill', 'fool', 'coward', 'puke-stocking', 'leper', 'skainsmate', 'infection',
             'simpleton', 'villain', 'miscreant'];
 
@@ -19,16 +19,16 @@ insultGen = () => {
     console.log(`I don't have the words to describe you! Mr. Shakespeare will have to help me. THOU ART A ${insultAdj1[x].toUpperCase()}, ${insultAdj2[y].toUpperCase()} ${insultNoun[z].toUpperCase()}!`);
 };
 
-flatteryAdj1 = ['sweet', 'divine', 'lovely', 'inspiring', 'completely', 'refined', 'angelic', 'wise', 'soothing', 'wholesome', 'magical',
+const flatteryAdj1 = ['sweet', 'divine', 'lovely', 'inspiring', 'completely', 'refined', 'angelic', 'wise', 'soothing', 'wholesome', 'magical',
             'mellow', 'thoughtful', 'good', 'fresh', 'delightful', 'melodious', 'golden', 'endearing', 'strong', 'delicate', 'powerful',
             'courteous', 'kind'];
 
-flatteryAdj2 = ['free-spirited', 'heaven-created', 'pure-hearted', 'clear-seeing', 'brave-spirited', 'rose-cheeked', 'silken-haired',
+const flatteryAdj2 = ['free-spirited', 'heaven-created', 'pure-hearted', 'clear-seeing', 'brave-spirited', 'rose-cheeked', 'silken-haired',
             'sugar-coated', 'lilac-scented', 'brave-willed', 'doe-eyed', 'honey-tongued', 'much-favored', 'heart-enflaming', 'clever-witted',
             'all-knowing', 'ivory-smooth', 'well-wishing', 'thunder-darting', 'mood-lifting', 'will-bucking', 'even-tempered', 'well-fated',
             'spirit-chosen'];
 
-flatteryNoun = ['god', 'goddess', 'saint', 'star', 'knight', 'sprite', 'lionheart', 'angel', 'hero', 'ornament', 'book', 'champion', 'delight',
+const flatteryNoun = ['god', 'goddess', 'saint', 'star', 'knight', 'sprite', 'lionheart', 'angel', 'hero', 'ornament', 'book', 'champion', 'delight',
             'force', 'nymph', 'warrior', 'muse', 'apple-john', 'melody', 'inspiration', 'wit', 'true-penny', 'valentine', 'love'];
 
 //should this piece of flattery be properly punctuated?
@@ -49,7 +49,6 @@ dayType = ['happy', 'peaceful', 'wonderful', 'inspirational', 'bright', 'excitin
 dayToHave = () => {
     let x = Math.floor(Math.random() * (dayType.length + 1));
     let y = dayType[x].charAt(0);
-    console.log(y);
     if (y === 'a' || y === 'e' || y === 'i' || y === 'o' || y === 'u' || y === 'y') {
         console.log (`I hope you have an ${dayType[x]} day!`)
     } else {
